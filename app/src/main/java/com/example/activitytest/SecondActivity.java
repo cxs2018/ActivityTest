5305462b12eaad0b3e859703cc783bee4021855d
cuixuesen <2821436626@qq.com>
2022-07-19 23:27:45 +0800

    feat: 学习activity

---
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
