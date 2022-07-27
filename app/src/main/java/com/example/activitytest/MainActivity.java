958e3b527eea54588a91e03d8b043ec726968d79
cuixuesen <2821436626@qq.com>
2022-07-27 23:41:07 +0800

    feat: 省市县联动

--- app/src/main/java/com/example/activitytest/MainActivity.java
+++ app/src/main/java/com/example/activitytest/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.activitytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("main", "in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
